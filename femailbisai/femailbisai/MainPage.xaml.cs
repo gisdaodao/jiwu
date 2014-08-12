ecfdc73ef6cc37671e491fc633ce236daf0e2fff
sodowp <pi*5=5.12>
2014-08-12 17:59:03 +0800

    cc

---
+++ femailbisai/femailbisai/MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+using System.Xml.Linq;
+using System.Collections;
+using GoogleAds;
+using Microsoft.Phone.Tasks;
+
+namespace femailbisai
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        WebClient Pclient = new WebClient();
+        List<string> urls = new List<string>();
+        List<Info> items = new List<Info>();
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+            // 将 listbox 控件的数据上下文设置为示例数据
+            interstitialAd = new InterstitialAd("ca-app-pub-1598808565430684/3997460859");
+            // NOTE: You can edit the event handler to do something custom here. Once the
+            // interstitial is received it can be shown whenever you want.
+            interstitialAd.ReceivedAd += OnAdReceived;
+            AdRequest adRequest = new AdRequest();
+            adRequest.ForceTesting = false;
+            interstitialAd.LoadAd(adRequest);
+            Pclient.OpenReadCompleted += Pclient_OpenReadCompleted;
+            Pclient.OpenReadAsync(new Uri("https://raw.githubusercontent.com/gisdaodao/jiwu/master/jiewu/femalebisai.xml", UriKind.Absolute));          
+        }
+        private InterstitialAd interstitialAd;
+        private void OnAdReceived(object sender, AdEventArgs e)
+        {
+            Random P = new Random();
+            int I = P.Next(1, 10);
+            if (I > 5)
+            {
+                interstitialAd.ShowAd();
+            }
+        }
+        // 为 ViewModel 项加载数据
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            
+          
+        }
+
+        void Pclient_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            if(e.Error==null)
+            {
+                Stream stream = e.Result;
+                XElement p = XElement.Load(stream);
+            
+                XName xname = XName.Get("url");
+                IEnumerable<XElement> nodes= p.Descendants(xname).ToList<XElement>();
+                foreach(var a in nodes)
+                {
+                    urls.Add(a.Value);
+                }
+                Deployment.Current.Dispatcher.BeginInvoke(() => { lstbox.ItemsSource = urls; });
+                XName xitemname = XName.Get("item");
+                IEnumerable<XElement> itemnodes = p.Descendants(xitemname).ToList<XElement>();
+                foreach (var b in itemnodes)
+                {
+
+                    items.Add(new Info() { text = b.FirstAttribute.Value, info = b.LastAttribute.Value });
+                }
+                Deployment.Current.Dispatcher.BeginInvoke(() => { longlistbox.ItemsSource = items; });
+            }
+        
+        }
+
+        private void joinAppbar_Click(object sender, EventArgs e)
+        {
+            MarketplaceReviewTask task = new MarketplaceReviewTask();
+            task.Show();
+        }
+
+      
+    }
+    public class Info
+    {
+        public string text{get;set;}
+          public string info{get;set;}
+    }
+}
--- jiewu/jiewu/MainPage.xaml.cs
+++ jiewu/jiewu/MainPage.xaml.cs
@@ -19,6 +19,19 @@ namespace jiewu
 
             // 将 listbox 控件的数据上下文设置为示例数据
             DataContext = App.ViewModel;
+            jiewu.MediaOpened += jiewu_MediaOpened;
+            jiewu.MediaFailed += jiewu_MediaFailed;
+            jiewu.Source = new Uri("http://k.youku.com/player/getFlvPath/sid/7407696443988122cc477_00/st/flv/fileid/03000201004B3A6448D4E8002A7D49A3B7D432-3995-8003-BDA3-56F231B6D7C1?K=6d037192416c82252411e676&ctype=12&ev=1&oip=2095617680&token=5709&ep=dSaUHU%2BEUMoE5irXgD8bZX%2BxcHAIXP4J9h%2BFg9JjALsgOuvOnzrVz%2Bm0O%2FdCFvtodVYCGZry2deTbkMWYfNKoG0Q106oOvqSioHg5ahVspIDZ2swBsqktVSbRDD4",UriKind.Absolute);
+        }
+
+        void jiewu_MediaFailed(object sender, ExceptionRoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void jiewu_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         // 为 ViewModel 项加载数据
